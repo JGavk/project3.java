a9e368e0373ae22ababf5fb823bb968a4a352622
JGavk <119267651+JGavk@users.noreply.github.com>
2023-10-26 15:33:22 -0500

    Create Square.java
    Abstract class for the square just to create the parameters for the cube

---
+++ Square.java
@@ -0,0 +1,40 @@
+package model;
+
+import java.awt.Graphics;
+import java.awt.Image;
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author juanp
+ */
+public abstract class Square {
+    
+    public ImageIcon image;
+    public String imageUrl;
+    public Square(String imageUrl, int width, int height) { 
+        ImageIcon originalImage = new ImageIcon(getClass().getResource(imageUrl));
+        Image scaledImage = originalImage.getImage().getScaledInstance(width, height, Image.SCALE_SMOOTH);
+        image = new ImageIcon(scaledImage);
+        }
+    public abstract void draw(Graphics g, int x, int y);
+}   
+    // Subclase cuadrado
+    class Cubo extends Square {
+        public Cubo (String imageUrl, int width, int height) {
+            super(imageUrl, width, height);
+            
+    }
+        public void setImageUrl(String newImageUrl) {
+            imageUrl = newImageUrl;
+            ImageIcon originalImage = new ImageIcon(getClass().getResource(imageUrl));
+            Image scaledImage = originalImage.getImage().getScaledInstance(image.getIconWidth(), image.getIconHeight(), Image.SCALE_SMOOTH);
+            image = new ImageIcon(scaledImage);
+        }
+
+    @Override
+    public void draw(Graphics g, int x, int y) {
+        g.drawImage(image.getImage(), x, y, null);
+    }
+}
+    
