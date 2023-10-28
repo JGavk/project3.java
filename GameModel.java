55f991ba385cd7ffd6d7b600dd634e7ac7d7bb33
JGavk <119267651+JGavk@users.noreply.github.com>
2023-10-28 16:13:49 -0500

    Update GameModel.java
    Changes from the other class

--- GameModel.java
+++ GameModel.java
@@ -7,31 +7,39 @@ import java.util.List;
 
 
 public class GameModel {
-    private List<List<Square>> squareArray;
+    private List<Cubo> squareArray;
     private FirstController myController;
     public GameModel() {
+        
         squareArray = new ArrayList<>();
-        squareArray.add(new ArrayList<>());
-        squareArray.add(new ArrayList<>());
-        squareArray.add(new ArrayList<>());
-        squareArray.add(new ArrayList<>());
-    
-
     
         Cubo cubo1 = new Cubo("/images/image1.jpg",100,100);
-        squareArray.get(0).add(cubo1);
+        squareArray.add(cubo1);
         Cubo cubo2 = new Cubo("/images/image2.jpg",100,100);
-        squareArray.get(1).add(cubo2);
+        squareArray.add(cubo2);
         Cubo cubo3 = new Cubo("/images/image3.jpg",100,100);
-        squareArray.get(2).add(cubo3);
+        squareArray.add(cubo3);
         Cubo cubo4 = new Cubo("/images/image4.jpg",100,100);
-        squareArray.get(3).add(cubo4);
+        squareArray.add(cubo4);
+        Cubo cubo5 = new Cubo("/images/image4.jpg",100,100);
+        squareArray.add(cubo5);
+        Cubo cubo6 = new Cubo("/images/image3.jpg",100,100);
+        squareArray.add(cubo6);
+        Cubo cubo7 = new Cubo("/images/image2.jpg",100,100);
+        squareArray.add(cubo7);
+        Cubo cubo8 = new Cubo("/images/image1.jpg",100,100);
+        squareArray.add(cubo8);
     }
-      public void setFirstController(FirstController myController) {
+       
+    public void setFirstController(FirstController myController) {
        this.myController = myController;
   }
-       public List<List<Square>> getSquareArray(){
+    public void setSquareArray(List<Cubo> squareArray){
+        this.squareArray= squareArray;
+    
+    }
+    public List<Cubo> getSquareArray(){
           
-           return squareArray;}
+        return squareArray;}
 }
 
