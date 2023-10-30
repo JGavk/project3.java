91d8c31eef8b21d50a73389cf33bbf4c074f8a68
JGavk <119267651+JGavk@users.noreply.github.com>
2023-10-29 19:00:05 -0500

    Update FirstController.java
    To update the controller connected to the GameView, it nows displays correctly the array of images and change the image to a particular by time

--- FirstController.java
+++ FirstController.java
@@ -3,21 +3,25 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.List;
 import view.FirstView;
-import view.GameView; 
+import view.GameView;
 import model.GameModel;
 import model.Cubo;
+
+import javax.swing.*;
 
 public class FirstController {
     private FirstView frame;
     private GameView game;
     private GameModel model;
     private List<Cubo> squareArray;
-    
-    
+    private Timer imageTimer;
+
     public FirstController() {
         this.frame=frame;
-        this.game=game;
-   }    
+        this.game= game;
+
+
+   }
 
     public void setFirstView(FirstView frame) {
         this.frame=frame;
@@ -25,14 +29,34 @@ public class FirstController {
     public void setSquareArray() {
         model.setSquareArray();
     }
+    private void startTimer(){
+        imageTimer = new Timer(3000, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                changeRandomImage();
+            }
+        });
+        imageTimer.start();
+    }
+    private void changeRandomImage() {
+        if (!squareArray.isEmpty()) {
+            this.model.resetImage();
+            this.game.updateImages(this.model.getSquareArray());
+        }
+    }
     public void chargeGameFrame() {
-        GameView game = new GameView();
+        game = new GameView();
         model = new GameModel();
+        game.setFirstController(this);
         this.setSquareArray();
         this.squareArray = model.getSquareArray();
         game.displaySquareArray(squareArray);
         frame.dispose();
+        this.startTimer();
+    }
 
+    public void updateScore() {
+        this.game.pressMe(this.model.checkMatch());
     }
 
     public void setGameView(GameView game) {
