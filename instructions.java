3fb9c737a7d7a304d67cf11d4fecf86aacee0fc5
sotomilos <119267681+sotomilos@users.noreply.github.com>
2023-10-30 11:14:09 -0500

    Create instructions.java

---
+++ instructions.java
@@ -0,0 +1,12 @@
+package controller;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import view.*;
+
+public class instruccionController {
+    public static void EventoSalir(){
+        NewJFrame newframe = new NewJFrame();
+        
+        newframe.setVisible(true);
+    }
+}
