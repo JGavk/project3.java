d65c5741d417ec22bc1d6efa2cef919a7b1b95df
sotomilos <119267681+sotomilos@users.noreply.github.com>
2023-10-30 00:51:48 -0500

    Update FirstView.java
    Add  final JFrame

--- FirstView.java
+++ FirstView.java
@@ -1,79 +1,102 @@
-
 package view;
-import controller.FirstController;
-import java.awt.Color;
-import java.awt.FlowLayout;
-import java.awt.Font;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
-import javax.swing.*;
+public class NewJFrame extends javax.swing.JFrame {
 
-
-public class FirstView extends JFrame implements ActionListener {
-    private JLabel jBackground;
-    private JLabel jContent;
-    private JButton btnJoin, btnDo, btnInstructions;
-    private ImageIcon backgroundIcon;
-    private FirstController myController;
-        public FirstView(){
-        startComponent();
-    }
-    private void startComponent(){
-        // Configuración de pantalla y fondo de pantalla--------------------------------------------
-        backgroundIcon = new ImageIcon(this.getClass().getResource("/images/background.jpg"));
-        jContent = new JLabel(backgroundIcon);
-        jContent.setSize(1200, 800);
-        setTitle("Popsicle");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(1200,800);
-        setLocationRelativeTo(null); //Toma la locacion en el medio de pantalla
-        setVisible(true);
-        setResizable(false);
-        setLayout(null);
-        add(jContent);
-        //-------------------------------------------------------------------------------------------
-
-        btnJoin = new JButton("JUGAR");
-        btnJoin.setBounds(520,650, 150,35);
-        jContent.add(btnJoin);
-        btnJoin.setFocusPainted(false);  
-        btnJoin.setFont(new Font("Arial", Font.BOLD, 24));
-        btnJoin.setForeground(Color.WHITE);
-        btnJoin.setBorder(null);
-        btnJoin.setBackground(Color.BLACK);
-        btnJoin.addActionListener(this);
-        //------------------------------------------------------------------------------------------- 
-        
-        btnDo = new JButton("Para qué sirve");
-        btnDo.setBounds(850, 580, 200, 60);
-        jContent.add(btnDo);
-        btnDo.setFont(new Font("Arial",Font.BOLD,26));
-        btnDo.setBorder(null);
-        btnDo.setFocusPainted(false);
-        btnDo.setBackground(Color.GRAY);
-        //--------------------------------------------------------------------------------------------
-        
-        btnInstructions = new JButton ("Cómo Jugar");
-        btnInstructions.setBounds(185,580,200,60);
-        jContent.add(btnInstructions);
-        btnInstructions.setFont(new Font("Arial",Font.BOLD,26));
-        btnInstructions.setBorder(null);
-        btnInstructions.setFocusPainted(false);
-        btnInstructions.setBackground(Color.GRAY);
-        
-        //--------------------------------------------------------------------------------------------
-
+    /**
+     * Creates new form FirstView
+     */
+    public NewJFrame() {
+        initComponents();
+        this.setLocationRelativeTo(null);
     }
 
-    public void setFirstController(FirstController myController) {
-        this.myController = myController;
-    }
-    
-    public void actionPerformed(ActionEvent e){
-        if(e.getSource()==btnJoin){
-            myController.chargeGameFrame();
-            
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">                          
+    private void initComponents() {
+
+        btnIngresar = new javax.swing.JButton();
+        btnInstrucciones = new javax.swing.JButton();
+        btnParaQSirve = new javax.swing.JButton();
+        background = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setMaximumSize(new java.awt.Dimension(1200, 800));
+        setUndecorated(true);
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        btnIngresar.setBackground(new java.awt.Color(0, 0, 0));
+        btnIngresar.setFont(new java.awt.Font("Arial", 1, 24)); // NOI18N
+        btnIngresar.setForeground(new java.awt.Color(255, 255, 255));
+        btnIngresar.setText("JUGAR");
+        btnIngresar.setMaximumSize(new java.awt.Dimension(150, 35));
+        btnIngresar.setMinimumSize(new java.awt.Dimension(150, 35));
+        getContentPane().add(btnIngresar, new org.netbeans.lib.awtextra.AbsoluteConstraints(540, 630, 140, 40));
+
+        btnInstrucciones.setBackground(new java.awt.Color(153, 153, 153));
+        btnInstrucciones.setFont(new java.awt.Font("Arial", 1, 24)); // NOI18N
+        btnInstrucciones.setForeground(new java.awt.Color(51, 51, 51));
+        btnInstrucciones.setText("CÓMO JUGAR");
+        btnInstrucciones.setMaximumSize(new java.awt.Dimension(200, 60));
+        btnInstrucciones.setMinimumSize(new java.awt.Dimension(200, 60));
+        btnInstrucciones.setOpaque(true);
+        btnInstrucciones.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnInstruccionesActionPerformed(evt);
+            }
+        });
+        getContentPane().add(btnInstrucciones, new org.netbeans.lib.awtextra.AbsoluteConstraints(240, 570, 210, -1));
+
+        btnParaQSirve.setBackground(new java.awt.Color(153, 153, 153));
+        btnParaQSirve.setFont(new java.awt.Font("Arial", 1, 24)); // NOI18N
+        btnParaQSirve.setText("PARA QUÉ SIRVE?");
+        getContentPane().add(btnParaQSirve, new org.netbeans.lib.awtextra.AbsoluteConstraints(840, 570, -1, -1));
+
+        background.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/background.jpg"))); // NOI18N
+        getContentPane().add(background, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 0, -1, -1));
+
+        pack();
+    }// </editor-fold>                        
+
+    private void btnInstruccionesActionPerformed(java.awt.event.ActionEvent evt) {                                                 
+        // TODO add your handling code here:
+    }                                                
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new NewJFrame().setVisible(true);
+            }
+        });
     }
+
+    // Variables declaration - do not modify                     
+    private javax.swing.JLabel background;
+    private javax.swing.JButton btnIngresar;
+    private javax.swing.JButton btnInstrucciones;
+    private javax.swing.JButton btnParaQSirve;
+    // End of variables declaration                   
 }
