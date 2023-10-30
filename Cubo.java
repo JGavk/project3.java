cb88683c0b227efb2c085f5af63d83a5f20e8cc9
JGavk <119267651+JGavk@users.noreply.github.com>
2023-10-29 19:01:31 -0500

    Update Cubo.java
    update of cubo class

--- Cubo.java
+++ Cubo.java
@@ -5,15 +5,40 @@ import javax.swing.ImageIcon;
 
 public class Cubo {
     private ImageIcon image;
+    private String imagePath;
     private int width;
     private int height;
+
+    public void setImage(String imageUrl) {
+        ImageIcon originalImage = new ImageIcon(getClass().getResource(imageUrl));
+        image = new ImageIcon(originalImage.getImage().getScaledInstance(width, height, Image.SCALE_SMOOTH));
+        this.imagePath = imageUrl;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
 
     public Cubo(String imageUrl, int width, int height) {
         ImageIcon originalImage = new ImageIcon(getClass().getResource(imageUrl));
         image = new ImageIcon(originalImage.getImage().getScaledInstance(width, height, Image.SCALE_SMOOTH));
         this.width = width;
         this.height = height;
+        this.imagePath = imageUrl;
     }
+
+    public String getImagePath() {
+        return this.imagePath;
+    }
+
+    /*public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }*/
+
 
     public ImageIcon getImage() {
         return image;
