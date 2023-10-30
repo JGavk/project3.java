ecd83825d43ff46e16ce48e7b0b3ba9ddd6abbde
sotomilos <119267681+sotomilos@users.noreply.github.com>
2023-10-30 11:17:14 -0500

    Create WTGIFController.java

---
+++ WTGIFController.java
@@ -0,0 +1,11 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import view.*;
+
+public class WTGIFController {
+    public static void EventoSalir(){
+        NewJFrame newframe = new NewJFrame();
+        
+        newframe.setVisible(true);
+    }
+}
