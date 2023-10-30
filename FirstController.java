2feebfb348116a14649dc8463528edbd395ede40
JGavk <119267651+JGavk@users.noreply.github.com>
2023-10-30 16:47:01 -0500

    Update FirstController.java

--- FirstController.java
+++ FirstController.java
@@ -6,40 +6,49 @@ import view.FirstView;
 import view.GameView;
 import model.GameModel;
 import model.Cubo;
+import view.ImproveView;
+import view.InstructionsView;
 
 import javax.swing.*;
 
 public class FirstController {
     private FirstView frame;
+
+    private ImproveView improveFrame;
+    private InstructionsView instructionsView;
     private GameView game;
     private GameModel model;
     private List<Cubo> squareArray;
     private Timer imageTimer;
 
     public FirstController() {
-        this.frame=frame;
-        this.game= game;
-
-
+        this.frame = frame;
+        this.game = game;
+        this.improveFrame = improveFrame;
+        this.instructionsView = instructionsView;
    }
-    public static void EventoInstrucciones(){
-        InstructionsJFrame newframe = new InstructionsJFrame();
-        
-        newframe.setVisible(true);
-    }
-    
-    public static void EventoParaQSirve(){
-        WTGIFJFrame newframe = new WTGIFJFrame();
-        
-        newframe.setVisible(true);
-    }
-    
-      public void setFirstView(FirstView frame) {
+
+//Controla la primera pestaña
+    public void setFirstView(FirstView frame) {
         this.frame=frame;
     }
+//Controla el cambio desde ventana de función vuelta a FirstFrame    
+    public void getFirstView(){
+        frame = new FirstView();
+        frame.setFirstController(this);
+        improveFrame.dispose();
+    }
+ //Controla el cambio desde Instrucción  y vuelta a FirstFrame     
+    public void getFirstView2(){
+        frame = new FirstView();
+        frame.setFirstController(this);
+        instructionsView.dispose();
+    }
+//Obtiene el arreglo    
     public void setSquareArray() {
         model.setSquareArray();
     }
+//Comienza el temporizador de imagenes    
     private void startTimer(){
         imageTimer = new Timer(3000, new ActionListener() {
             @Override
@@ -49,12 +58,15 @@ public class FirstController {
         });
         imageTimer.start();
     }
+//Controla el cambio de imagenes comunicando el modelo con la vista    
     private void changeRandomImage() {
         if (!squareArray.isEmpty()) {
             this.model.resetImage();
             this.game.updateImages(this.model.getSquareArray());
         }
     }
+
+//Carga el juego con toda su logica de cambio en la vista    
     public void chargeGameFrame() {
         game = new GameView();
         model = new GameModel();
@@ -65,25 +77,33 @@ public class FirstController {
         frame.dispose();
         this.startTimer();
     }
-
+//Controla el cambio de puntuacion y vida
     public void updateScore() {
         this.game.pressMe(this.model.checkMatch());
         this.model.resetButton();
         this.game.putImages(this.model.getSquareArray(), true);
     }
 
-    public void setGameView(GameView game) {
-     this.game=game;
-
-    }
 
    public Object getGameModel() {
         return model;
     }
 
-
-
-
+//Controla el cambio a ventana de función
+    public void chargeImproveView() {
+        improveFrame = new ImproveView();
+        improveFrame.setFirstController(this);
+        frame.dispose();
+    }
+//Controla el cambio a la ventana de instrucciones
+    public void chargeInstructionsView() {
+        instructionsView = new InstructionsView();
+        instructionsView.setFirstController(this);
+        frame.dispose();
+    }
 }
+        
+        
+        
         
         
