d2505f688746b474ce0a709b63a02cb29b8c13ab
JGavk <119267651+JGavk@users.noreply.github.com>
2023-10-31 10:06:11 -0500

    Update GameModel.java

--- GameModel.java
+++ GameModel.java
@@ -1,4 +1,8 @@
-
+/*
+Miguel Angel Soto Espitia 2240019
+Juan Pablo Puerta Gaviria 2240033
+18/10/23-31/10/23
+*/
 package model;
 
 import controller.FirstController;
@@ -22,6 +26,7 @@ public class GameModel {
     public void setFirstController(FirstController myController) {
        this.myController = myController;
   }
+  //Crea el arreglo base de objetos de tipo Cubo
     public void setSquareArray(){
         Cubo cubo1 = new Cubo("/images/image1.jpg", 100, 100);
         squareArray.add(cubo1);
@@ -34,6 +39,12 @@ public class GameModel {
         this.squareArray= squareArray;
     
     }
+//Liga la funcionalidad
+    public  void resetButton(){
+        this.squareArray = new ArrayList<>();
+        this.setSquareArray();
+    }
+//Da la logica de cambio de imagen en cambio de objeto de memoria
     public void resetImage(){
         this.squareArray.forEach(obj -> {
             //ImageIcon newImage = new ImageIcon(this.btnImages.get(new Random().nextInt(this.btnImages.size())));
@@ -42,11 +53,12 @@ public class GameModel {
             //obj.setImagePath(newImage);
         });
     }
+    //Obtiene el arreglo
     public List<Cubo> getSquareArray(){
           
         return squareArray;
     }
-
+//Da la logica de imagenes iguales
     public Boolean checkMatch() {
         AtomicReference<Boolean> match = new AtomicReference<>(Boolean.FALSE);
 
@@ -55,11 +67,7 @@ public class GameModel {
                 .filter(cubo -> !newitemSet.add(cubo.getImagePath()))
                 .count();
 
-        this.squareArray.forEach(obj -> {
-            System.out.println("IMAGE  " + obj.getImagePath());
-        });
 
-        System.out.println("The list of duplicate Items: " + numOfMatches);
 
         if (numOfMatches >= 1) {
             match.set(Boolean.TRUE);
@@ -68,6 +76,7 @@ public class GameModel {
         return match.get();
     }
 }
+
 
 
        
